bc9d328383d883d298d58b40d8fc63355647b0ed
Robert Comp <26robertcomp@usd266.com>
2024-03-28 12:12:50 -0500

    did printing demos

--- Unit1/1PrintingAssignment.py
+++ Unit1/1PrintingAssignment.py
@@ -1,21 +1,29 @@
 print("########## 1.1.1 ##########")
 # your code for exercise 1.1.1 goes here
+print(":>")
 
 print("########## 1.1.2 ##########")
 # modify the code below for exercise 1.1.2
-print("Doc")
-print("Grumpy")
-print("Sleepy")
 print("Bashful")
+print("doc")
+print("dopey")
+print("grumpy")
 print("Happy")
-print("Sneezy")
-print("Dopey")
+print("sleepy")
+print("sneezy")
 
 print("########## 1.1.3 ##########")
 # your code for exercise 1.1.3 goes here
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print ("Merrily,merrily, merrily, merrily,")
+print ("Life is but a dream")
 
 print("########## 1.1.4 ##########")
 # your code for exercise 1.1.4 goes here
+print ("minutes in a year")
+print (60*24*365)
 
 print("########## 1.1.5 ##########")
 # your code for exercise 1.1.5 goes here
+print ('print("hello world")')
