9a90e6494747f5944bf107e5bd4a291dc8ffe8b6
Robert Comp <26robertcomp@usd266.com>
2024-04-01 12:18:30 -0500

    1.3

--- Unit1/3VariablesAssignment.py
+++ Unit1/3VariablesAssignment.py
@@ -10,25 +10,34 @@ skill3 = "programming"
 level3 = "semiprofessional"
 lower = 2000
 upper = 3000
+print ("my name is " + name + ", I am 20 years old")
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print ("my skills are")
+print ("- ",skill1 + " (" + str(level1) + ")")
+print ("- ",skill2+ ( " (") + str(level2) + ")")
+print ("- ",skill3+ " (" + str(level3) + ")")
+print ("")
+print ("I am looking for a job with a salary of" + " " + str(lower) + ("-") + str (upper) + " " + ("dollars per month"))
+
+
 
 
 print("########## 1.3.2 ##########")
 x = 27
 y = 15
+print ("")
+print ("x+y=", x + y, )
+print ("x-y=", x-y,)
+print ("x*y=", x*y,)
+print ("x/y=",x/y,)
+print ("")
 
 
 print("########## 1.3.3 ##########")
-print(5)
-print(" + ")
-print(8)
-print(" - ")
-print(4)
-print(" = ")
-print(5 + 8 - 4)
+print(5 , end="")
+print(" + ", end="")
+print(8 , end="")
+print(" - ", end="")
+print(4, end="")
+print (" = ", end="")
+print(5+8-4)
