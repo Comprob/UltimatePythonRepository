0ccf3aaaa120e9269d89281e5210e55073e30a7f
Robert Comp <26robertcomp@usd266.com>
2024-04-03 11:50:59 -0500

    1/4

--- Unit1/4ArithmeticAssignment.py
+++ Unit1/4ArithmeticAssignment.py
@@ -1,23 +1,52 @@
 print("########## 1.4.1 ##########")
+nu=int (input("type a number: "))
+print (nu*5)
 
 print("########## 1.4.2 ##########")
+name=input ("what is your name:")
+year=int (input ("what year were you born in:"))
+age=2024-year
+print ("Hi " + name + ", you will be"  + " "+str(age)+ " " +  "old at the end of the year 2024" ) 
 
 print("########## 1.4.3 ##########")
+day=int (input("how many second in select amount of days"))
+print (day*24*60*60)
 
 print("########## 1.4.4 ##########")
 # Fix the code
-number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
+number1 = int(input("Please type in the first number: "))
+number2 = int(input("Please type in the second number: "))
+number3 = int(input("Please type in the third number: "))
 
-product = number * number * number
+product = number1 * number2 * number3
 
 print("The product is", product)
 
 print("########## 1.4.5 ##########")
+nu1=int (input ("number 1:"))
+nu2=int (input ("number 2"))
+print ("the sum of the numbers:" + str  (nu1+nu2))
+print ("the product of the numbers are" + str  (nu1*nu2))
 
 print("########## 1.4.6 ##########")
+nu1=int (input( "number 1:"))
+nu2=int (input ("number 2:"))
+nu3=int (input ("number 3:"))
+nu4=int (input ("number 4:"))
+total= nu1+nu2+nu3+nu4
+print ("the sum of the numbers is" + " " + str (total) + " " + "and the mean is" + " " + str (total/4))
 
 print("########## 1.4.7 ##########")
+epw=int (input("how many time do you eat at the student cafetria?"))
+asl=int (input("the price of a typical student lunch?"))
+gr=int (input("how much money do you spend on groceries in a week?"))
+sch= epw*asl+gr
+print ("")
+print ("average food expenditure")
+print ("daily: $" + str (sch/7) )
+print ("weekly: $ " + str (sch))
+# print("########## 1.4.8 ##########")
 
-print("########## 1.4.8 ##########")
+# nus= int(input("how many students on the course?"))
+# des= int(input("desired group size?"))
+# print (" number of groups formed: " + str(nus//des))
