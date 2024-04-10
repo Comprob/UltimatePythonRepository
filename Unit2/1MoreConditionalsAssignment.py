5ae416c32364b6dcfc57f9cd837e4e2a4bd7a7ad
Robert Comp <26robertcomp@usd266.com>
2024-04-10 12:16:25 -0500

    2.1

--- Unit2/1MoreConditionalsAssignment.py
+++ Unit2/1MoreConditionalsAssignment.py
@@ -1,11 +1,55 @@
 # print("########## 2.1.1 ##########")
 
+# word=input ("please type a word: ")
+# print(len("There are a total of:" + word))
+# print ("thank you")
+
 # print("########## 2.1.2 ##########")
+
+# temp= float(input("please type in a temperture: "))
+# print ("integar part: ", int(temp)) 
+# print ("the decmial part: ", (temp-int(temp)))
 
 # print("########## 2.1.3 ##########")
 
+# age=int (input("How old are you: "))
+# if age<18:
+#     print("you may not vote")
+# else:
+#     print("you may vote")
+
 # print("########## 2.1.4 ##########")
+# nu1=int (input("Please type in the first number: "))
+# nu2=int (input("please type in a second number"))
+# if nu1<nu2:
+#     print(str (nu2) + " is bigger than " + str (nu1))
+# elif nu1>nu2:
+#     print( str (nu1) + " is bigger than " +  str (nu2))
+# else:
+#     print("The numbers are equal!")
 
 # print("########## 2.1.5 ##########")
 
+# print ("Person 1")
+# name1=input ("name: ")
+# n1=int (input("age: "))
+# print ("person 2")
+# name2=input ("Name 2: ")
+# n2=int (input("age: "))
+# if n1>n2:
+#     print("the elder is " + name1)
+# elif n2>n1:
+#     print("the elder is " + name2)
+# else:
+#     print(name1 + " and " + name2 + " are the same age" )
+
 # print("########## 2.1.6 ##########")
+
+# word1=input("Please type in the 1st word: ")
+# word2=input("Please type in the 2nd word: ")
+# if word1>word2:
+#     print (word1 + " comes alphabetically last")
+# elif word1<word2:
+#     print (word2 + " comes alphabetically last")
+# else:
+#     print("you gave the same word twice")
