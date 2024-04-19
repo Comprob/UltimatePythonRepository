2b178c163d29688b189477b30e0958a7d4a8bf34
Robert Comp <26robertcomp@usd266.com>
2024-04-19 11:21:04 -0500

    3.1

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,54 @@
 # ========== 3.1.1 ==========
-
+# nu=0
+# sen=0
+# while nu<30:
+#     nu=nu+2
+#     print(nu)
 
 # ========== 3.1.2 ==========
 
-# print("Are you ready?")
-# number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
-# print("Go!")
+# nu=int(input("Please type in a number: "))
+# while nu>0:
+#     print (nu)
+#     nu=nu-1
+#     if nu==0:
+#         print("now!")
 
 # ========== 3.1.3 ==========
 
+# nu=int(input("upper limit: "))
+# while nu>1:
+#     nu=nu-1
+#     print(nu)
 
 # ========== 3.1.4 ==========
 
+# un=int(input("upper limit: "))
+# n=1
+# while un>=n:
+#     print(n)
+#     n=n*2
 
 # ========== 3.1.5 ==========
 
+# up=int(input("upper limit "))
+# base=int(input("Base: "))
+# nu=1
+# while nu<up:
+#     print(nu)
+#     nu=nu*base
+   
 
-# ========== 3.1.6 ==========
+    
+# ========== 3.1.6 ==========\
+
+# add=0
+# limit=int(input("limit: "))
+# sum=0
+
+# while sum<limit:
+#     add=add+1
+#     sum=sum+add
+#     # print("currently adding:", add, "and the sum has become", sum)
+# print(sum)
+
